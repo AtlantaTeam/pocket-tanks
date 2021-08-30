e6491455716b65707b5544cedbd85c361606c7e0
Dmitry Lyapin <lyapindm@yandex.ru>
2021-08-30 15:58:59 +0300

    страница таблица результатов

--- src/components/Pages/LeaderBoard/LeaderBoard.tsx
+++ src/components/Pages/LeaderBoard/LeaderBoard.tsx
@@ -1,3 +1,62 @@
+import { Input } from 'components/components/Forms/components/Input/Input';
+import { Title } from 'components/components/Title/Title';
 import React from 'react';
+import { Page } from '../components/Page/Page';
 
-export const LeaderBoard = () => <h2>Таблица результатов</h2>;
+import './LeaderBoard.css';
+
+const players = [
+    {
+        display_name: 'Super Tankist 9000',
+    },
+    {
+        display_name: 'Mario Abrams',
+    },
+    {
+        display_name: 'Серый волк',
+    },
+    {
+        display_name: 'Красная шапочка',
+    },
+    {
+        display_name: 'Магомед Магедов',
+    },
+    {
+        display_name: 'Player 1',
+    },
+    {
+        display_name: 'Player 1455',
+    },
+    {
+        display_name: 'Batya Ivanicvich',
+    },
+    {
+        display_name: 'Пуля',
+    },
+    {
+        display_name: 'Ракета',
+    },
+];
+
+export const LeaderBoard = () => (
+    <Page>
+        <div className="leader-board-container">
+            <div className="leader-board-container">
+                <Title
+                    className="title title_middle"
+                    text="Лучшие стрелки"
+                />
+            </div>
+            {
+                players.map((item, index) => (
+                    <Input
+                        key={item.display_name}
+                        placeholder=""
+                        className="input input_black input_leader-board"
+                        value={`${String(index + 1)}   ${item.display_name}`}
+                    />
+                ))
+            }
+        </div>
+    </Page>
+);
