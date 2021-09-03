01a79f22476169dcef43d6a56f27d681f06c3a2b
Dmitry Lyapin <lyapindm@yandex.ru>
2021-09-03 16:08:14 +0300

    stories

--- src/components/components/WeaponSelect/WeaponSelect.stories.tsx
+++ src/components/components/WeaponSelect/WeaponSelect.stories.tsx
@@ -13,23 +13,40 @@ export default {
 } as ComponentMeta<typeof WeaponSelect>;
 
 const Template: ComponentStory<typeof WeaponSelect> = (
+    args,
 ) => (
     <WrapperCenter className="wrapper-center wrapper-center_full-screen">
-        <WeaponSelect
-            weapon={[
-                { id: 1, name: 'Ядро' },
-                { id: 2, name: 'Ракета' },
-                { id: 3, name: 'Снаряд' },
-                { id: 4, name: 'Фугас' },
-                { id: 5, name: 'Пулемет' },
-                { id: 6, name: 'Ядро 9000' },
-                { id: 7, name: 'Ракета 850r4' },
-                { id: 8, name: 'Снаряд 55E81' },
-            ]}
-            label="Тип оружия"
-            listPosition="top"
-        />
+        <WeaponSelect weapon={args.weapon} label={args.label} listPosition={args.listPosition} />
     </WrapperCenter>
 );
 
-export const WeaponSelectTemplate = Template.bind({});
+export const WeaponSelectTemplateTop = Template.bind({});
+
+WeaponSelectTemplateTop.args = {
+    weapon: [
+        { id: 1, name: 'Ядро' },
+        { id: 2, name: 'Ракета' },
+        { id: 3, name: 'Снаряд' },
+        { id: 4, name: 'Фугас' },
+        { id: 5, name: 'Пулемет' },
+        { id: 6, name: 'Ядро 9000' },
+        { id: 7, name: 'Ракета 850r4' },
+        { id: 8, name: 'Снаряд 55E81' },
+    ],
+    label: 'Тип оружия',
+    listPosition: 'top',
+};
+
+export const WeaponSelectTemplateBottom = Template.bind({});
+
+WeaponSelectTemplateBottom.args = {
+    weapon: [
+        { id: 1, name: 'Ядро' },
+        { id: 2, name: 'Ракета' },
+        { id: 3, name: 'Снаряд' },
+        { id: 4, name: 'Фугас' },
+        { id: 5, name: 'Пулемет' },
+    ],
+    label: 'Тип оружия',
+    listPosition: 'bottom',
+};
