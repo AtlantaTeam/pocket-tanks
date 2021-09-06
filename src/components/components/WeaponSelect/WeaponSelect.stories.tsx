5666bbb999113f1da6fe953c13b09b6e91b94436
Dmitry Lyapin <52707043+Pelmenya@users.noreply.github.com>
2021-09-06 20:59:58 +0300

    Pocket tanks 3 (#13)
    * основные элементы дизайн системы
    
    * добавил template для авторизации. Использовал Formik и Yup
    
    * формы: SignIn, SignUp, OAuth
    
    * формы: SignIn, SignUp, OAuth
    
    * формы SignIn, SignUp, OAuth
    
    * rebase
    
    * Выпадающее меню с абсолютным позиционированием
    
    * Выпадающее меню с абсолютным позиционированием
    
    * роутинг
    
    * страницы  авторизации, регистрации
    
    * форма загрузки аватара
    
    * страница профиля
    
    * страница таблица результатов
    
    * поправил замечания
    
    * поправил замечания
    
    * Главная страница
    
    * Компонент Counter
    
    * Компонент Counter
    
    * Компонент WeaponSelect
    
    * Добавил позиционирование WeaponSelect
    
    * переделал все на absolute
    
    * stories

--- src/components/components/WeaponSelect/WeaponSelect.stories.tsx
+++ src/components/components/WeaponSelect/WeaponSelect.stories.tsx
@@ -5,6 +5,7 @@ import {
 } from '@storybook/react';
 
 import { WeaponSelect } from './WeaponSelect';
+import { WrapperCenter } from '../WrapperCenter/WrapperCenter';
 
 export default {
     title: 'POCKET-TANKS/components/WeaponSelect',
@@ -12,17 +13,40 @@ export default {
 } as ComponentMeta<typeof WeaponSelect>;
 
 const Template: ComponentStory<typeof WeaponSelect> = (
+    args,
 ) => (
-    <WeaponSelect
-        weapon={[
-            { id: 1, name: 'Ядро' },
-            { id: 2, name: 'Ракета' },
-            { id: 3, name: 'Снаряд' },
-            { id: 4, name: 'Фугас' },
-            { id: 5, name: 'Пулемет' },
-        ]}
-        label="Тип оружия"
-    />
+    <WrapperCenter className="wrapper-center wrapper-center_full-screen">
+        <WeaponSelect weapon={args.weapon} label={args.label} listPosition={args.listPosition} />
+    </WrapperCenter>
 );
 
-export const WeaponSelectTemplate = Template.bind({});
+export const WeaponSelectTemplateTop = Template.bind({});
+
+WeaponSelectTemplateTop.args = {
+    weapon: [
+        { id: 1, name: 'Ядро' },
+        { id: 2, name: 'Ракета' },
+        { id: 3, name: 'Снаряд' },
+        { id: 4, name: 'Фугас' },
+        { id: 5, name: 'Пулемет' },
+        { id: 6, name: 'Ядро 9000' },
+        { id: 7, name: 'Ракета 850r4' },
+        { id: 8, name: 'Снаряд 55E81' },
+    ],
+    label: 'Тип оружия',
+    listPosition: 'top',
+};
+
+export const WeaponSelectTemplateBottom = Template.bind({});
+
+WeaponSelectTemplateBottom.args = {
+    weapon: [
+        { id: 1, name: 'Ядро' },
+        { id: 2, name: 'Ракета' },
+        { id: 3, name: 'Снаряд' },
+        { id: 4, name: 'Фугас' },
+        { id: 5, name: 'Пулемет' },
+    ],
+    label: 'Тип оружия',
+    listPosition: 'bottom',
+};
