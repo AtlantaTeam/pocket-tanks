f808aad8077aebb53f86fc92ad68acb20f6cb13b
Andrey Korotayev <76525701+a-k-kord@users.noreply.github.com>
2021-10-25 14:25:22 +0300

    remove herokuapp  to pass tests.

--- src/constants/api-routes.ts
+++ src/constants/api-routes.ts
@@ -7,7 +7,7 @@ export const RESOURCES_BASE_URL = 'https://ya-praktikum.tech/api/v2/resources/';
 export const OAUTH_AUTHORIZE_URL = 'https://oauth.yandex.ru/authorize?response_type=code';
 
 // поменять при деплои в облако
-export const SERVER_URL = IS_DEV ? 'https://localhost:5000' : 'https://pocketanks.herokuapp.com';
+export const SERVER_URL = 'https://localhost:5000'; // IS_DEV ?  : 'https://pocketanks.herokuapp.com';
 
 export const AUTH_ROUTES = {
     GET_USER_INFO: '/auth/user',
